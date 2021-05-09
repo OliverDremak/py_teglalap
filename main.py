4c089084cd6bcb771df9bc501654bcba9885540f
OliverDremak <72332685+OliverDremak@users.noreply.github.com>
2021-05-09 10:51:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+a=int(input("Kérem adja meg az egyik oldalt:"))
+b=int(input("Kérem adja meg az másik oldalt:"))
+terulet=a*b
+kerulet=a*2+b*2
+print("A téglalap területe:",terulet)
+print("A téglalap kerülete:",kerulet)
